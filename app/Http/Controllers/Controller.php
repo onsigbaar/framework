87f20dc86510c988ff9cba8531a4d11d204dc233
rn <associate.ru@outlook.com>
2017-04-04 12:43:30 +0700

    Updated app directory

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Route;
 use Illuminate\Http\Request;
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
